107bdd81344804c7c2b2c920c7687bf995c94984
Gerhard Messelink <gerhard.messelink@kpn.com>
2025-04-19 22:01:33 +0200

    Add kunstrand.nl website

---
+++ kunstrand_nl/js/vanhoning.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+	// MAsonary PROJECTS
+	$('#masonary_container').masonry({
+	  columnWidth: 340,
+	  itemSelector: '.masonary_item',
+	  gutter: 20,
+	  isOriginLeft: false
+	});
+	
+/*
+	setMainWidth();
+
+	function setMainWidth(){
+		$("#content_container").css('width','800');
+	}
+	
+	$(window).resize(function() {
+		setMainWidth();
+	});	
+*/
+
+});
+
+
